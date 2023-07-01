c996a4b0db95a3dac80124f3271317d9c163c8b8
weatherpub <patrick@bayareamedia.net>
2023-06-30 20:19:50 -0700

    Adding minor comments

---
+++ app/src/main/java/com/example/android_actransit/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.android_actransit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // patrick
+    }
+}
