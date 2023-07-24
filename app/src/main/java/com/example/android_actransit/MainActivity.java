d18616ff9c2bb4bd68121c357f45b0d1ed3e84cf
weatherpub <patrick@bayareamedia.net>
2023-07-24 12:46:23 -0700

    RecyclerView and AsyncTask are now working!!!!!

--- app/src/main/java/com/example/android_actransit/MainActivity.java
+++ app/src/main/java/com/example/android_actransit/MainActivity.java
@@ -4,53 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
 import com.example.android_actransit.Adapters.StopsAdapter;
 import com.example.android_actransit.Helpers.StopsAsyncTask;
 import com.example.android_actransit.Models.StopsModel;
+import com.example.android_actransit.R.id;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
-    LinearLayoutManager layoutManager;
-    RecyclerView recyclerView;
-    StopsAdapter adapter;
-    ArrayList<StopsModel> items = new ArrayList<>();
+//    LinearLayoutManager layoutManager;
+
+    ArrayList<StopsModel> model;
+    private ProgressBar progressBar;
+    private StopsAsyncTask stopsAsyncTask;
+    private RecyclerView recyclerView;
 
     private ArrayList<StopsModel> initData() {
         ArrayList<StopsModel> items = new ArrayList<>();
+        /*
+        items.add(new StopsModel(40001, "Lake Shore", 12.0, 32.0, "now"));
+        items.add(new StopsModel(53001, "Grand Ave", 72.0, 22.0, "Later"));
+        items.add(new StopsModel(63333, "Cleveland Ave", 72.0, 22.0, "Sometime Today"));
 
-        items.add(new StopsModel(40001, "Lake Shore", 12.0, 32.0, "now"));
-        items.add(new StopsModel(53001, "Grand Ave", 72.0, 22.0, "Later"));
-        items.add(new StopsModel(40001, "Lake Shore", 12.0, 32.0, "now"));
-        items.add(new StopsModel(53001, "Grand Ave", 72.0, 22.0, "Later"));
-        items.add(new StopsModel(40001, "Lake Shore", 12.0, 32.0, "now"));
-        items.add(new StopsModel(53001, "Grand Ave", 72.0, 22.0, "Later"));
-        items.add(new StopsModel(40001, "Lake Shore", 12.0, 32.0, "now"));
-        items.add(new StopsModel(53001, "Grand Ave", 72.0, 22.0, "Later"));
-        items.add(new StopsModel(40001, "Lake Shore", 12.0, 32.0, "now"));
-        items.add(new StopsModel(53001, "Grand Ave", 72.0, 22.0, "Later"));
-        items.add(new StopsModel(40001, "Lake Shore", 12.0, 32.0, "now"));
-        items.add(new StopsModel(53001, "Grand Ave", 72.0, 22.0, "Later"));
-        items.add(new StopsModel(40001, "Lake Shore", 12.0, 32.0, "now"));
-        items.add(new StopsModel(53001, "Grand Ave", 72.0, 22.0, "Later"));
-        items.add(new StopsModel(40001, "Lake Shore", 12.0, 32.0, "now"));
-        items.add(new StopsModel(53001, "Grand Ave", 72.0, 22.0, "Later"));
-        items.add(new StopsModel(40001, "Lake Shore", 12.0, 32.0, "now"));
-        items.add(new StopsModel(53001, "Grand Ave", 72.0, 22.0, "Later"));
-        items.add(new StopsModel(40001, "Lake Shore", 12.0, 32.0, "now"));
-        items.add(new StopsModel(53001, "Grand Ave", 72.0, 22.0, "Later"));
-        items.add(new StopsModel(40001, "Lake Shore", 12.0, 32.0, "now"));
-        items.add(new StopsModel(53001, "Grand Ave", 72.0, 22.0, "Later"));
-        items.add(new StopsModel(40001, "Lake Shore", 12.0, 32.0, "now"));
-        items.add(new StopsModel(53001, "Grand Ave", 72.0, 22.0, "Later"));
-        items.add(new StopsModel(40001, "Lake Shore", 12.0, 32.0, "now"));
-        items.add(new StopsModel(53001, "Grand Ave", 72.0, 22.0, "Later"));
-        items.add(new StopsModel(40001, "Lake Shore", 12.0, 32.0, "now"));
-        items.add(new StopsModel(53001, "Grand Ave", 72.0, 22.0, "Later"));
-        items.add(new StopsModel(40001, "Lake Shore", 12.0, 32.0, "now"));
-        items.add(new StopsModel(53001, "Grand Ave", 72.0, 22.0, "Later"));
-        items.add(new StopsModel(40001, "Lake Shore", 12.0, 32.0, "now"));
-        items.add(new StopsModel(53001, "Grand Ave", 72.0, 22.0, "Later"));
-
+         */
+        //return objects;
         return items;
     }
     @Override
@@ -59,20 +40,31 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        ArrayList<StopsModel> items = initData();
-        getStops();
-
         recyclerView = findViewById(R.id.recyclerView);
 
-        layoutManager = new LinearLayoutManager(getApplicationContext());
+        model = initData();
 
-        recyclerView.setLayoutManager(layoutManager);
+        new StopsAsyncTask(MainActivity.this, recyclerView, progressBar, model).execute("https://api.actransit.org/transit/stops/?token=010910F3B6021AE11917D7EFDC2F26CD");
+//       ArrayList<StopsModel> list = new ArrayList<>();
 
-        adapter = new StopsAdapter(MainActivity.this, items);
+        /*
+        StopsAsyncTask stopsAsyncTask = new StopsAsyncTask(MainActivity.this, list);
+        stopsAsyncTask.execute("https://api.actransit.org/transit/stops/?token=010910F3B6021AE11917D7EFDC2F26CD");
+         */
+
+
+        /*
+        for(StopsModel sm: list){
+            Log.i("VIDEO-1000", sm.getName());
+        }
+
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
+
+        StopsAdapter adapter = new StopsAdapter(MainActivity.this, list);
 
         recyclerView.setAdapter(adapter);
-    }
-    public void getStops() {
-        new StopsAsyncTask(this, items).execute("https://api.actransit.org/transit/stops/?token=010910F3B6021AE11917D7EFDC2F26CD");
+        */
     }
 }
